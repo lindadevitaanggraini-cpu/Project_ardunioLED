cc548458b8ecd23dad7b1c74af226608965e83f6
Linda Devita Anggraini <lindadevitaanggraini@gmail.com>
2026-06-05 19:17:42 +0700

    Initial commit

---
+++ project_arduinoLED.ino
@@ -0,0 +1,45 @@
+int led_merah = 2;
+
+
+
+void setup() {
+ pinMode(led_merah, OUTPUT);
+
+
+
+ serial.begin(19200);
+}
+
+void loop() {
+  if (serial.available() > 0) {
+    char perintah = serial.read();
+
+    serial.print("Menerima: ");
+    serial.println(perintah);
+
+    //1= merah nyala (tanpa mematikan yang lain)
+    if(perintah == '1'){
+      digitalWrite(led_merah, HIGHT);
+    }
+
+    //2= kuning menyala
+
+
+
+    //3= hijau menyala
+
+
+
+
+
+
+   //4= semua mati
+
+
+
+
+
+   //5= semua nyala
+  }
+
+}
