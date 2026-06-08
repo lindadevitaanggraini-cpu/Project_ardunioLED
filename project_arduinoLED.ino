95990fcdb3b0228c5752c59292d07279a3090464
Elsa Melania Putri <melaniaelsa011@gmail.com>
2026-06-08 17:45:00 +0700

    penambahan led kuning - elsa

--- project_arduinoLED.ino
+++ project_arduinoLED.ino
@@ -1,12 +1,11 @@
 int led_merah = 2;
-int led_kuning = 4;
+
 int led_hijau = 6;
 
 void setup() {
  pinMode(led_merah, OUTPUT);
- pinMode(led_kuning, OUTPUT);
- pinMode(led_hijau, OUTPUT);
 
+ pinMode(led_hijau,OUTPUT);
  serial.begin(19200);
 }
 
@@ -22,25 +21,14 @@ void loop() {
       digitalWrite(led_merah, HIGHT);
     }
 
-    //2= kuning menyala
-    else if (perintah == '2'){
-       digitalWrite(led_kuning, HIGH);
-      }
-    //3= hijau menyala
-    else if (perintah == '3'){
-        digitalWrite(led_hijau, HIGH);
-      }
+  
    //4= semua mati
    else if (perintah == '4'){
    digitalWrite(led_merah, LOW);
-   digitalWrite(led_kuning, LOW);
-   digitalWrite(led_hijau, LOW);
-   }
+  
    //5= semua nyala
    else if (perintah == '5'){
    digitalWrite(led_merah, HIGH);
-   digitalWrite(led_kuning, HIGH);
-   digitalWrite(led_hijau, HIGH);
-   }
+  
   }
 }
